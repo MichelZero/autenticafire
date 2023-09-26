24467aae3fdeaa06b4f203f5625d0e82cba53f30
Michel <106674127+MichelZero@users.noreply.github.com>
2023-09-26 15:08:22 -0300

    up

---
+++ screens/TesteHome.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TesteHome = () => {
+  return (
+    <View>
+      <Text>TesteHome</Text>
+    </View>
+  )
+}
+
+export default TesteHome
+
+const styles = StyleSheet.create({})
