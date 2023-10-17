2652660cdacc76706254f980caa69e98529dc5d6
Michel <106674127+MichelZero@users.noreply.github.com>
2023-10-17 13:35:55 -0300

    up

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import '@firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTAe3TjsmPNQdmoaIisXNGD1WhjsJloB4",
+  apiKey: "AIzaSyDTAe3TjsmPNQdmoaIisXNGD1WhjsJlo",
   authDomain: "autentica-fire01.firebaseapp.com",
   projectId: "autentica-fire01",
   storageBucket: "autentica-fire01.appspot.com",
